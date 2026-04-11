f5fd978e75ffdecd720e88affc2ee8f4306da965
Nahuel Oroza <nahuel.oroza@gmail.com>
2026-04-11 14:14:33 -0300

    Adds aws template upload to bucket.

---
+++ src/upload_templates.sh
@@ -0,0 +1,3 @@
+aws s3 cp github_oidc_provider.yaml s3://my-cf-templates/github_oidc_provider.yaml
+aws s3 cp github_policies.yaml s3://my-cf-templates/github_policies.yaml
+aws s3 cp github_roles.yaml s3://my-cf-templates/github_roles.yaml
